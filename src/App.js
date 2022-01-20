fed65517d7677052132b121c6058182affdb2686
petter <petter0915@gmail.com>
2022-01-20 14:02:40 -0500

    Ajustes link de navegación TopBar

--- src/App.js
+++ src/App.js
@@ -5,12 +5,14 @@ import LandingPage from "./pages/LandingPage/Index";
 import Home from "./pages/Home/Index";
 import Visitados from "./pages/Profile/Show/Visitados";
 import { Route, Routes } from "react-router-dom";
+import { Place } from "./pages/ShowPlace/Place";
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MainLayout/>}>
         <Route path="" element={<LandingPage/>} />
         <Route path="home" element={<Home/>} />
+        <Route path="place" element={<Place/>} />
         <Route path="/profile/show/visitados" element={<Visitados/>} />
       </Route>
     </Routes>
--- src/components/TopBar.js
+++ src/components/TopBar.js
@@ -1,29 +1,24 @@
-import * as React from 'react';
-import { Container, Nav, Navbar } from 'react-bootstrap';
-import LoginModal from './LoginModal';
-import logo from '../assets/logo.svg'
-import { Link } from 'react-router-dom';
-const pages = ['Home', 'About', 'Features', 'Pricing', 'Gallery', 'Team'];
-const settings = ['Profile', 'Account', 'Dashboard', 'Logout'];
-
+import * as React from "react";
+import { Container, Nav, Navbar } from "react-bootstrap";
+import LoginModal from "./LoginModal";
+import logo from "../assets/logo.svg";
+import { Link } from "react-router-dom";
+const pages = ["Home", "About", "Features", "Pricing", "Gallery", "Team"];
+const settings = ["Profile", "Account", "Dashboard", "Logout"];
 
 const TopBar = () => {
-  
   return (
     <Navbar bg="light" variant="light">
       <Container>
         <Navbar.Brand>
           <Link to="/">
-            <img src={logo} alt="asdasd"/>
+            <img src={logo} alt="asdasd" />
           </Link>
         </Navbar.Brand>
         <Nav className="me-auto">
-          
-          <Nav.Link>
-            <Link to="/home">Home</Link>
-          </Nav.Link>
-          <Nav.Link href="#features">Features</Nav.Link>
-          <Nav.Link href="#pricing">Pricing</Nav.Link>
+          <Link className="nav-link" to="/home"> Home</Link>
+          <Link className="nav-link" to="/place"> Place </Link>
+
         </Nav>
         <LoginModal></LoginModal>
       </Container>
