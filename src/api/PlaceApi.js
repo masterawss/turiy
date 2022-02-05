95891d0450612824e6b3d18b4c580bed64c2883f
masterawss <masterawss@gmail.com>
2022-02-05 10:15:37 -0500

    json para mockup de datos

---
+++ src/api/PlaceApi.js
@@ -0,0 +1,18 @@
+export async function getPlaces(){
+    // LEER JSON
+
+    // FILTRAR RESULTADOS
+
+    // RETORNAR RESULTADOS
+
+    return;
+}
+export async function getPlace(id){
+    // LEER JSON
+
+    // FILTRAR RESULTADOS
+
+    // RETORNAR RESULTADOS
+
+    return;
+}
