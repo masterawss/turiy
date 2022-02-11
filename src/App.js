6d3786894b484b7969f81ff66b68e1056f34b9c0
masterawss <masterawss@gmail.com>
2022-02-11 18:36:37 -0500

    Rutas editadas

--- src/App.js
+++ src/App.js
@@ -13,11 +13,40 @@ import Review from "./pages/Place/Show/Review";
 import { Place } from "./pages/Place/Place";
 import { ShowGuide } from "./pages/Profile/Show/ShowGuide";
 
+// HOME --------------------------
+// home/
+
 // PLACE --------------------------
 // place/           -> INDEX
 // place/:id        -> SHOW
 // place/:id/create -> CREATE
 // place/:id/edit   -> EDIT
+
+// USER ------------------------
+// user/:id        -> SHOW
+
+// PROFILE ------------------------
+// configuration    -> Obtiene siempre el id del usuario de sesión
+// checkout         -> Obtiene siempre el id del usuario de sesión
+// guide_register   -> Obtiene siempre el id del usuario de sesión
+
+/* <Routes>
+  <Route path="/" element={<MainLayout/>}>
+    <Route path="" element={<LandingPage/>} />
+    <Route path="home" element={<Pages/Home/Index/>} />
+    
+    <Route path="place" element={<Pages/Place/Index/>} />
+    <Route path="place/:id" element={<Pages/Place/Show/>} />
+
+    <Route path="user/:id" element={<Pages/User/Show/>} />
+    
+    <Route path="configuration" element={<Pages/Profile/Configuration/>} />
+    <Route path="checkout" element={<Pages/Profile/Checkout/>} />
+    <Route path="guide_register" element={<Pages/Profile/GuideRegister/>} />
+
+  </Route>
+</Routes> */
+
 
 function App() {
   return (
@@ -30,6 +59,11 @@ function App() {
         <Route path="/profile/show/saved" element={<Saved/>} />
         <Route path="/place/show/review" element={<Review/>} />
         <Route path="/profile/show/guia" element={<ShowGuide/>} />
+        
+        <Route path="perfil:id" element={<RegisterGuide/>} />
+        <Route path="registro_guia" element={<RegistroGuia/>} />
+        <Route path="pagos" element={<Checkout/>} />
+        <Route path="validacion" element={<Preform/>} />
       </Route>
     </Routes>
   );
--- src/api/GuideApi.js
+++ src/api/GuideApi.js
@@ -1,7 +1,11 @@
+import db from 'db/places.json'
+export async function getGuides(){
+    return db.users;
+}
 export async function getGuide(id){
-    // LEER JSON
-
     // FILTRAR RESULTADOS
+    let users = db.users.filter(users => users.id == id )
 
     // RETORNAR RESULTADOS
-}
+    return users;
+} 
